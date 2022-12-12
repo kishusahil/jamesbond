e3cda7e9e86ad2097d964eb06c0c4d229c7d0d0b
kisha <kisha@DESKTOP-MN6RKUO>
2022-12-12 12:24:20 +0530

    commit

---
+++ src/main/java/kishan/projectmaven/App.java
@@ -0,0 +1,13 @@
+package kishan.projectmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
